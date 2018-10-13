63b2643dcfbc7848f53e3b3c26cd70ad130fd6e8
Carlos Vargas <csv17@live.unc.edu>
2018-10-13 13:00:24 -0400

    plug-in finally working and adding information to the site

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-console.log("Script connected")
+var deleteLog = false;
+
+$(document).ready(function () {
+	$('#pagepiling').pagepiling({
+		menu: '',
+		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+		sectionsColor: ['#FC9D9A', '#FE4365', '#F9CDAD', '#C8C8A9', '#83AF9B', '#FC9D9A'],
+		loopTop: true,
+		loopBottom: true,
+		navigation: {
+			'position': 'right',
+			   'tooltips': ['Home', 'Dashboard', 'History', 'History 2', 'Story', 'Quiz']
+		   },
+		afterRender: function(){
+			$('#pp-nav').addClass('custom');
+		},
+		afterLoad: function(anchorLink, index){
+			if(index>1){
+				$('#pp-nav').removeClass('custom');
+			}else{
+				$('#pp-nav').addClass('custom');
+			}
+		}
+	});
+});
+
+console.log('Scripts js connected');
